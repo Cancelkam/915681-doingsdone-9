5adfece788afa3c115cbac6496250118e1e6ea33
Elena <cancel.kam@gmail.com>
2019-05-11 01:08:27 +0300

    Исправила первый запрос

--- queries.sql
+++ queries.sql
@@ -1,16 +1,12 @@
 
--- Дамп данных таблицы doingsdone.projects: ~5 rows (приблизительно)
-/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+
 INSERT INTO `projects` (`id`, `project_name`, `user_id`) VALUES
 	(1, 'Входящие', '2'),
 	(2, 'Учеба', '2'),
 	(3, 'Работа', '1'),
 	(4, 'Домашние дела', '2'),
 	(5, 'Авто', NULL);
-/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
 
--- Дамп данных таблицы doingsdone.tasks: ~7 rows (приблизительно)
-/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
 INSERT INTO `tasks` (`id`, `status`, `title`, `file_link`, `due_date`, `date_create`, `user_id`, `project_id`) VALUES
 	(1, 1, 'Собеседование в IT компании', NULL, '2019-01-12', '2019-05-08 22:09:23', 1, 3),
 	(2, 0, 'Выполнить тестовое задание', NULL, '2018-12-21', '2019-05-08 22:11:16', 1, 3),
@@ -18,17 +14,13 @@ INSERT INTO `tasks` (`id`, `status`, `title`, `file_link`, `due_date`, `date_cre
 	(4, 0, 'Встреча с другом', NULL, '2019-12-22', '2019-05-08 22:17:55', 2, 1),
 	(5, 0, 'Купить корм для кота', NULL, NULL, '2019-05-08 22:18:37', 2, 4),
 	(6, 0, 'Заказать 2 пиццы', NULL, NULL, '2019-05-08 22:18:52', 2, 4);
-/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
 
--- Дамп данных таблицы doingsdone.users: ~2 rows (приблизительно)
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
 INSERT INTO `users` (`id`, `reg_date`, `email`, `name`, `password`) VALUES
 	(1, '2019-05-08 23:28:14', '111@ya.ru', 'Andrey', '123123'),
 	(2, '2019-05-08 23:28:43', '222@ya.ru', 'Alyona', '321321');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
-SELECT project_name FROM project JOIN user ON user_id=2 /*получить список из всех проектов для одного пользователя*/
-SELECT COUNT(1) FROM task WHERE project_id=3 /*посчитать количество задач в каждом проекте*/
+SELECT project_id,COUNT(*) project_cnt FROM tasks JOIN users ON tasks.user_id = users.id where users.id=2 group BY project_id  /*получить список из всех проектов для одного пользователя*//*посчитать количество задач в каждом проекте*/
 SELECT title FROM tasks WHERE project_id=1 /*получить список из всех задач для одного проекта*/
 UPDATE doingsdone.tasks SET status=1 WHERE id=1; /*пометить задачу как выполненную*/
 UPDATE doingsdone.tasks SET title='Заказать 3 пиццы' WHERE id=6; /*обновить название задачи по её идентификатору*/
---
+++ schema.sql
@@ -0,0 +1,37 @@
+
+CREATE DATABASE IF NOT EXISTS `doingsdone`;
+USE `doingsdone`;
+
+CREATE TABLE IF NOT EXISTS `projects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_name` varchar(50) DEFAULT NULL,
+  `user_id` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`project_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` tinyint(1) DEFAULT '0' COMMENT 'число (1 или 0), означающее, была ли выполнена задача',
+  `title` varchar(1000) NOT NULL COMMENT 'название задачи',
+  `file_link` varchar(1000) DEFAULT NULL COMMENT 'ссылка на файл',
+  `due_date` date DEFAULT NULL COMMENT 'дата, до которой задача должна быть выполнена',
+  `date_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(11) DEFAULT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `title` (`title`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `reg_date` datetime DEFAULT NULL COMMENT 'дата регистрации пользователя',
+  `email` varchar(100) DEFAULT NULL,
+  `name` varchar(30) NOT NULL,
+  `password` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='Атрибуты пользователей';

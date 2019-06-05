3c733e5f6cb720052052b3c04763e4bca0fdbcc5
Elena <cancel.kam@gmail.com>
2019-06-05 07:55:30 +0300

    Добавлена фильтрация задач

--- index.php
+++ index.php
@@ -35,19 +35,51 @@ if (isset($_GET['project_id'])) {
         header("HTTP/1.1 404 Not Found");
         die();
     }
-    // Получит список задач по конкретному проекту
 
 }
 
-$sql = 'SELECT title, status AS done, due_date AS date, file_link FROM tasks WHERE ' . ( $is_project_isset ? 'project_id =' . $project_id . ' AND ': "" ) . 'user_id =' . $user_id;
+if (isset($_GET['check']) && isset($_GET['task_id'])) {
+
+    $check=(int) $_GET['check'];
+    $task_id=(int) $_GET['task_id'];
+    $sql = 'UPDATE tasks SET status = ' . $check . ' WHERE id=' . $task_id . ' AND user_id=' . $user_id;
+    $result = mysqli_query($con, $sql);
+    test_result($result, $sql);
+}
+
+// if (isset($_GET['filter'])) {
+    switch ($_GET['filter']){
+        case 'day':
+            $sql='SELECT title, status AS done, due_date AS date, file_link, id FROM tasks WHERE ' . ( $is_project_isset ? 'project_id =' . $project_id . ' AND ': "" ) . 'user_id =' . $user_id . ' AND due_date=CURDATE()';
+            break;
+        case 'tomorrow':
+            $sql = 'SELECT title, status AS done, due_date AS date, file_link, id FROM tasks WHERE ' . ( $is_project_isset ? 'project_id =' . $project_id . ' AND ': "" ) . 'user_id =' . $user_id . ' AND due_date = CURDATE() + INTERVAL 1 DAY';
+            break;
+        case 'overdue':
+            $sql = 'SELECT title, status AS done, due_date AS date, file_link, id FROM tasks WHERE ' . ( $is_project_isset ? 'project_id =' . $project_id . ' AND ': "" ) . 'user_id =' . $user_id . ' AND due_date < CURDATE()';
+            break;
+        default:
+            $sql='SELECT title, status AS done, due_date AS date, file_link, id FROM tasks WHERE ' . ( $is_project_isset ? 'project_id =' . $project_id . ' AND ': "" ) . 'user_id =' . $user_id;
+    }
+// }
+
+
+
+// $sql = 'SELECT title, status AS done, due_date AS date, file_link, id FROM tasks WHERE ' . ( $is_project_isset ? 'project_id =' . $project_id . ' AND ': "" ) . 'user_id =' . $user_id;
 $result = mysqli_query($con, $sql);
 test_result($result, $sql);
 $tasks_list = mysqli_fetch_all($result,MYSQLI_ASSOC);
 
+$param = $_GET;
+unset($param['filter']);
+$url = '/' . pathinfo(__FILE__,PATHINFO_BASENAME) . "?" . http_build_query($param);
+
+
 $page_content = include_template('index.php',[
 'projects' => $projects_list,
 'doings'=> $tasks_list,
-'show_complete_tasks' => $show_complete_tasks
+'show_complete_tasks' => $show_complete_tasks,
+'url' => $url
 ]);
 
 $layout_content = include_template('layout.php', [
--- templates/index.php
+++ templates/index.php
@@ -2,41 +2,38 @@
                     <h2 class="content__main-heading">Список задач</h2>
 
                     <form class="search-form" action="index.php" method="post" autocomplete="off">
-                        <input class="search-form__input" type="text" name="" value="" placeholder="Поиск по задачам">
+                        <input class="search-form__input" type="text" name="search" value="" placeholder="Поиск по задачам">
 
                         <input class="search-form__submit" type="submit" name="" value="Искать">
                     </form>
 
                     <div class="tasks-controls">
                         <nav class="tasks-switch">
-                            <a href="/" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
-                            <a href="/" class="tasks-switch__item">Повестка дня</a>
-                            <a href="/" class="tasks-switch__item">Завтра</a>
-                            <a href="/" class="tasks-switch__item">Просроченные</a>
+                            <a href="<?= $url ?>&filter=all" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
+                            <a href="<?= $url ?>&filter=day" class="tasks-switch__item">Повестка дня</a>
+                            <a href="<?= $url ?>&filter=tomorrow" class="tasks-switch__item">Завтра</a>
+                            <a href="<?= $url ?>&filter=overdue" class="tasks-switch__item">Просроченные</a>
                         </nav>
 
                         <label class="checkbox">
                             <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
 
 
-                            <input class="checkbox__input visually-hidden show_completed" type="checkbox"
-                                <?php if ($show_complete_tasks === 1): ?>checked<?php endif?>>
+                            <input class="checkbox__input visually-hidden show_completed" type="checkbox">
 
                             <span class="checkbox__text">Показывать выполненные</span>
                         </label>
                     </div>
                     <table class="tasks">
                         <?php foreach ($doings as $value):  ?>
-                            <?php if ($show_complete_tasks === 1 || $value['done'] === 0): ?>
-                            <tr class="tasks__item task
-                            <?= $value['done'] === 1 ? 'task--completed': '' ?>
-                            <?= isImportantTask($value['date'],24) ? 'task--important' : '' ?>
-                            ">
+                            <?php if ($show_complete_tasks === 1 || $value['done'] == 0): ?>
+                            <tr class="tasks__item task <?= $value['done'] == 1 ? 'task--completed ': '' ?><?= isImportantTask($value['date'],24) ? 'task--important' : '' ?>">
 
                                 <td class="task__select">
                                     <label class="checkbox task__checkbox">
                                         <input class="checkbox__input visually-hidden task__checkbox" type="checkbox"
-                                            value="1">
+                                        <?= $value['done'] == 1 ? 'checked' : "" ?>
+                                            value="<?= $value['id'] ?>">
 
                                         <span class="checkbox__text"><?=htmlspecialchars($value['title'],ENT_QUOTES); ?></span>
 
@@ -52,5 +49,4 @@
                             </tr>
                             <?php endif ?>
                         <?php endforeach ?>
-                        <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
                     </table>
